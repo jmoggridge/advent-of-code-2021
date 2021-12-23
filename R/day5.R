38d733458c025b91332e61166d9f59b1ac2e6c35
jmoggridge <ja.moggridge@gmail.com>
2021-12-23 01:15:11 -0500

    d5 p2

--- R/day5.R
+++ R/day5.R
@@ -60,41 +60,102 @@ plot_diagram <- function(df){
 parse_data <- function(file){
   tibble(vents = readLines(file)) |> 
     separate(col = vents, sep = ' -> |,', into = c('x1', 'y1', 'x2', 'y2')) |> 
-    mutate(vent = as.factor(row_number()),
-           across(x1:y2, as.numeric)) |> 
-    mutate(slope = (y2-y1)/(x2-x1)) |> 
+    mutate(
+      vent = as.factor(row_number()),
+      across(x1:y2, as.numeric),
+      slope = (y2-y1)/(x2-x1)
+    ) |> 
     relocate(vent, x1, x2, y1, y2)
   
 }
 
-expand_path <- function(df) {
-  expand_grid(x = df$x1:df$x2,  y=df$y1:df$y2)
-}
-
-horizontal_and_vertical_paths <- function(df){
+horizontal_and_vertical_points <- function(df){
   df |> 
     filter(slope %in% c(0, -Inf, Inf)) |> 
     select(-slope) |> 
     group_by(vent) |> 
     nest(vector = c(x1,x2,y1,y2)) |> 
-    mutate(points = map(vector, expand_path))
+    mutate(points = map(vector, function(df) {
+      expand_grid(x = df$x1:df$x2,  y=df$y1:df$y2)
+    }))
 }
 
 ####
-
-df <- parse_data('data/day5.txt')
-# plot_diagram(df)
-
-paths <- horizontal_and_vertical_paths(df)
-
-# find overlap
-paths |> 
+parse_data('data/day5.txt') |> 
+  horizontal_and_vertical_points() |> 
   select(-vector) |> 
   unnest(cols = c(points)) |> 
+  # find overlap
   group_by(x,y) |> 
   summarize(overlap = n()) |> 
   filter(overlap >=2) |> 
   nrow()
+# 8622
+
+
+#---------------------------------------------------------------------------
+
+# --- Part Two ---
+
+#  Unfortunately, considering only horizontal and vertical lines
+#  doesn't give you the full picture; you need to also consider 
+#  diagonal lines.
+
+#  Because of the limits of the hydrothermal vent mapping system,
+#  the lines in your list will only ever be horizontal, vertical, 
+#  or a diagonal line at exactly 45 degrees. In other words:
+
+# An entry like 1,1 -> 3,3 covers points 1,1, 2,2, and 3,3.
+# An entry like 9,7 -> 7,9 covers points 9,7, 8,8, and 7,9.
+
+# Considering all lines from the above example would now produce the following diagram:
+# 
+# 1.1....11.
+# .111...2..
+# ..2.1.111.
+# ...1.2.2..
+# .112313211
+# ...1.2....
+# ..1...1...
+# .1.....1..
+# 1.......1.
+# 222111....
+
+# You still need to determine the number of points where at least two lines overlap. In the above example, this is still anywhere in the diagram with a 2 or larger - now a total of 12 points.
+# 
+# Consider all of the lines. At how many points do at least two lines overlap?
+
+
+diagonal_points <- function(df){
+  df |> 
+    filter(slope %in% c(1, -1)) |> 
+    group_by(vent) |> 
+    nest(vector = c(x1,x2,y1,y2)) |> 
+    mutate(points = map(
+      .x = vector,  
+      .f = function(df) {
+        tibble(x = seq(df$x1, df$x2), y = seq(df$y1, df$y2))
+      })
+    ) |> 
+    ungroup() |> 
+    unnest(points) |> 
+    select(x, y)
+}
+
+
+
+parse_data('data/day5.txt') |> 
+  horizontal_and_vertical_points() |> 
+  ungroup() |> 
+  select(points) |> 
+  unnest(points) |> 
+  bind_rows(diagonal_points(df)) |> 
+  count(x,y) |> 
+  filter(n >=2) |> 
+  nrow()
+#22037
+
+
 
 
 
